3061c1fa1c6ca5acf537dae4f639745da813e97b
YonasTewabe <yonastewabe21@gmail.com>
2024-03-29 05:24:07 -0500

    change fetch into axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,43 @@ import HomePage from "./Pages/HomePage";
 import Layout from "./Components/Layout";
 import JobsPage from "./Pages/JobsPage";
 import NotFoundPage from "./Pages/NotFoundPage";
-import JobPage, {jobLoader} from "./Pages/JobPage";
+import JobPage, { jobLoader } from "./Pages/JobPage";
 import AddJobPage from "./Pages/AddJobPage";
 import EditJobPage from "./Pages/EditJobPage";
 import ViewReportPage from "./Pages/ViewReportPage";
-
+import axios from "axios";
 
 const App = () => {
-//Add new job
+  // Add new job
   const addJob = async (newJob) => {
-    // eslint-disable-next-line no-unused-vars
-    const res = await fetch('/api/jobs', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newJob),
-    });
-    return;
-    }
-  //Deleting job
+    await axios.post('/api/jobs', newJob);
+  };
+
+  // Deleting job
   const deleteJob = async (id) => {
-    // eslint-disable-next-line no-unused-vars
-    const res = await fetch(`/api/jobs/${id}`, {
-      method: 'DELETE'
-    });
-    return;
-  }
+    await axios.delete(`/api/jobs/${id}`);
+  };
 
-
-  //updating job
+  // Updating job
   const updateJob = async (job) => {
-    // eslint-disable-next-line no-unused-vars
-    const res = await fetch(`/api/jobs/${job.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(job),
-    });
-    return;
-  }
+    await axios.put(`/api/jobs/${job.id}`, job);
+  };
 
-
-    const router = createBrowserRouter(
-      createRoutesFromElements(
-      <Route path ='/' element={<Layout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/report' element={<ViewReportPage />} />
-      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
-      <Route path='/edit-job/:id' element={<EditJobPage updatedJobSubmit={updateJob} />} loader={jobLoader} />
-      <Route path='/job/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
-      <Route path='*' element={<NotFoundPage />} />
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/report' element={<ViewReportPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/edit-job/:id' element={<EditJobPage updatedJobSubmit={updateJob} />} loader={jobLoader} />
+        <Route path='/job/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
-    );
-  return <RouterProvider router={router} />
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
