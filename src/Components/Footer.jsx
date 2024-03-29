70d61d6269ae524aac94d67e6a03a6dd9feb5b99
YonasTewabe <yonastewabe21@gmail.com>
2024-03-29 04:32:24 -0500

    landing page

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <footer className="bg-background text-white py-4 text-center 
+         bottom-100 w-full">
+        <p>&#169; 2024 All Rights Reserved</p>
+      </footer>
+      
+     );
+}
+ 
+export default Footer;
