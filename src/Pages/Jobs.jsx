4c678086ec63e4a0a9ebc393df7153cad94d6744
YonasTewabe <yonastewabe21@gmail.com>
2024-04-13 17:24:52 -0500

    change in naming format

---
+++ src/Pages/Jobs.jsx
@@ -0,0 +1,11 @@
+import JobListings from '../Components/JobListings';
+
+const Jobs = () => {
+  return (
+    <div className=' px-4 py-6'>
+    <JobListings />
+    </div>
+  )
+}
+
+export default Jobs
