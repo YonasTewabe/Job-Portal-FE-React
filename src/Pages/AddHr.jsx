6ebbb0eef8469a4a643bc4105389bb3392ad81f3
YonasTewabe <yonastewabe21@gmail.com>
2024-05-02 15:38:08 -0500

    comments and logs correction

--- src/Pages/AddHr.jsx
+++ src/Pages/AddHr.jsx
@@ -86,7 +86,7 @@ const AddHr = () => {
 
   return (
     <>
-      {myRole === "Admin" ? (
+      {myRole === "admin" ? (
         <div className="flex justify-center items-center h-screen">
           <div className="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <form onSubmit={handleSubmit} className="max-w-sm mx-auto mt-8">
