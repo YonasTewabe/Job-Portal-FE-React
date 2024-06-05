2489130f396e46c328b7f9999e98cbe6b7e861a8
YonasTewabe <yonastewabe21@gmail.com>
2024-06-05 13:59:50 -0500

    adjust UI

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return ( 
         <footer className="bg-background md-flex text-white py-4 text-center 
-         absolute bottom-100 w-full">
+         absolute min-h-screen w-full">
         <p>&#169; 2024 All Rights Reserved!</p>
         <div className="mt-4">
                 <Link to="/contact" className="text-white mx-2">Contact Us</Link>
